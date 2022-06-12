3148bc004a597be9443b886109315b5bbaec6b74
Santhanabalan <bala2102002@gmail.com>
2022-06-12 16:44:18 +0530

    Front end Final

--- crm1/main/urls.py
+++ crm1/main/urls.py
@@ -5,7 +5,4 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('innerpage/', views.innerpage, name='innerpage'),
-    path('login/', views.login, name='login'),
-    path('portfolio/', views.portfolio, name='portfolio'),
 ]
--- crm1/main/views.py
+++ crm1/main/views.py
@@ -2,9 +2,3 @@ from django.shortcuts import render
 
 def index (request): 
     return render(request, 'main/index.html')
-def innerpage (request): 
-    return render(request, 'main/inner-page.html')
-def login (request): 
-    return render(request, 'main/login.html')
-def portfolio (request): 
-    return render(request, 'main/portfolio-details.html')
