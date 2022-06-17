b9b709b4fad95b09055d0ed2f7776d186bb8cb67
Santhanabalan <bala2102002@gmail.com>
2022-06-18 00:43:09 +0530

    Update settings.py
    https://stackoverflow.com/questions/45391082/heroku-server-error-500-when-debug-false-whitenoise-could-not-find-style-c

--- crm1/settings.py
+++ crm1/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.staticfiles',
 
     'accounts.apps.AccountsConfig',
 
@@ -179,4 +179,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = '*********'
 EMAIL_HOST_PASSWORD = '*********'
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(),staticfiles=False)
